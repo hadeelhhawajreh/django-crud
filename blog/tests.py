e690d5fd7dabe3b15e99ab00ae98dd089e7711d5
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2021-01-05 19:17:17 +0200

    DJANGO CRUD + TEST

--- blog/tests.py
+++ blog/tests.py
@@ -1,66 +1,40 @@
-from django.test import TestCase
-
+from django.test import TestCase,SimpleTestCase
 # Create your tests here.
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-from .models import Snack
+from .models import ModelBlog
 
 # Create your tests here.
 
-class SnacksCRUDTests(TestCase):
+class CRUDTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            username = 'ahmad',
-            email = 'ahmad@ahmad.com',
-            password = '123456ahmad'
+            username = 'hadeel',
+            email = 'hadeel@gmail.com',
+            password = '123456'
         )
-        self.snack = Snack.objects.create(
-            name = 'chocolate',
-            rank = 10,
-            eater = self.user
+        self.snack = ModelBlog.objects.create(
+            title = 'Book',
+            author = self.user,
+            body= 'body ',
         )
 
-    def test_snacks_page_status(self):
+    def test_page_status(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
-    def test_snack_details_status(self):
-        response = self.client.get(reverse('snack_details', args='1'))
+    def test_details_status(self):
+        response = self.client.get(reverse('details', args='1'))
         self.assertEqual(response.status_code, 200)
 
-    def test_snack_details_content(self):
-        response = self.client.get(reverse('snack_details', args='1'))
-        self.assertContains(response, 'chocolate')
+    def test_details_content(self):
+        response = self.client.get(reverse('details', args='1'))
+        self.assertContains(response, 'Book')
 
-    def test_snack_update(self):
-        response = self.client.post(reverse('snack_update', args='1'), {
-            'rank': 8,
+    def test_update(self):
+        response = self.client.post(reverse('update_blog', args='1'), {
+            'author': 'hadeel',
         })
-        self.assertContains(response, 8)
-        self.assertNotContains(response, 10)
-
-
-# class SnacksTests(TestCase):
-#     def test_snacks_page_status(self):
-#         # visit the snacks list page
-#         url = reverse('home')
-#         # '/snacks/'
-#         # print(url)
-#         # get the response
-#         response = self.client.get(url)
-#         actual = response.status_code
-#         expected = 200
-#         self.assertEqual(expected, actual)
-
-#     def test_not_found(self):
-#         url = '/snacks/hello'
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, 404)
-
-#     def test_details_page_template(self):
-#         url = reverse('snack_details', args='1')
-#         print(url)
-#         response = self.client.get(url)
-#         print(response)
-#         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'hadeel')
+        self.assertNotContains(response, 'admin')
